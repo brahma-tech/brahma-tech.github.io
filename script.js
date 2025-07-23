9387e2d7b096a52867cd0c0da5bd663b0a644ee7
brahma-tech <anshumanthegoat@gmail.com>
2025-07-23 05:58:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Back to Top Button
-const backToTop = document.getElementById('backToTop');
-window.addEventListener('scroll', () => {
-  backToTop.style.display = window.scrollY > 300 ? 'block' : 'none';
-});
-backToTop.addEventListener('click', () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
